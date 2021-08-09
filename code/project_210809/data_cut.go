b8630768b838f7dce10686890d67f92eb93e4235
wereter31 <wereter31@naver.com>
2021-08-09 15:18:07 +0900

    Golang XML Parser

---
+++ code/project_210809/data_cut.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type measInfo struct {
+	XMLName xml.Name `xml:"measInfo"`
+	//Job     string   `xml:"job,attr"`
+	//GranPeriod string `xml:"granPeriod,attr,attr"`
+	//RepPeriod string `xml:"repPeriod,attr"`
+	MeAsType[] measType `xml:"measType,attr"`
+	//MeAsValue[] measValue `xml:"measValue,attr"`
+}
+
+type measType struct {
+	Key   string `xml:"p,attr"`
+	Value string `xml:",chardata"`
+}
+
+type measValue struct {
+	XMLName xml.Name `xml:"measValue"`
+	Key   string `xml:"p,attr"`
+	Value float32 `xml:",floatdata"`
+}
+
+func main() {
+	// xml 파일 오픈
+	fp, err := os.Open("/home/thkim/GolandProjects/project_210809/data_cut.xml")
+	if err != nil {
+		panic(err)
+	}
+	defer fp.Close()
+
+	// xml 파일 읽기
+	data, err := ioutil.ReadAll(fp)
+
+	// xml 디코딩
+	var pmInfo measInfo
+	err = xml.Unmarshal(data, &pmInfo)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(pmInfo)
+}
+
