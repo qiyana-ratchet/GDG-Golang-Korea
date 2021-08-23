c6bff664e4b3c16d3d873fd5068b0602a2f2a443
qiyana-ratchet <wereter31@naver.com>
2021-08-23 15:55:08 +0900

    Make new data collector using Prometheus node exporter

---
+++ code/pm_data.go
@@ -0,0 +1,132 @@
+package collector
+
+import (
+	"encoding/xml"
+	"github.com/go-kit/log"
+	"github.com/prometheus/client_golang/prometheus"
+	"io/ioutil"
+	"os"
+)
+
+var pmDesc = prometheus.NewDesc(
+	prometheus.BuildFQName(namespace, "pm", "data"),
+	"THIS IS SAMPLE TEST FOR PROMETHEUS METRICS BY TAEHYUN KIM",
+	[]string{
+		"SAMPLETEST1",
+		//라벨 이름은 영문이어야 합니다 (혹은 영문+숫자) -김태현
+	},
+	nil,
+)
+
+type pmCollector struct {
+	logger   log.Logger
+}
+type measDataFile struct {
+	XMLName    xml.Name   `xml:"measDataFile"`
+	FileHeader fileHeader `xml:"fileHeader"`
+	MeasData   measData   `xml:"measData"`
+	FileFooter fileFooter `xml:"fileFooter"`
+}
+type fileHeader struct {
+	XMLName           xml.Name   `xml:"fileHeader"`
+	FileFormatVersion string     `xml:"fileFormatVersion,attr"`
+	VendorName        string     `xml:"vendorName,attr"`
+	DnPrefix          string     `xml:"dnPrefix,attr"`
+	FileSender        fileSender `xml:"fileSender"`
+	MeasData          measData   `xml:"measData"`
+}
+type fileSender struct {
+	XMLName    xml.Name `xml:"fileSender"`
+	SenderName string   `xml:"senderName,attr"`
+	SenderType string   `xml:"senderType,attr"`
+}
+type fileFooter struct {
+	XMLName  xml.Name `xml:"fileFooter"`
+	MeasData measData `xml:"measData"`
+}
+type measData struct {
+	XMLName    xml.Name   `xml:"measData"`
+	MeasEntity measEntity `xml:"measEntity"`
+	MeasInfo   []measInfo `xml:"measInfo"`
+	BeginTime  string     `xml:"beginTime,attr"`
+	EndTime    string     `xml:"endTime,attr"`
+}
+type measEntity struct {
+	XMLName xml.Name `xml:"measEntity"`
+	Key     string   `xml:"localDn,attr"`
+	Key2    string   `xml:"swVersion,attr"`
+}
+type measInfo struct {
+	XMLName    xml.Name   `xml:"measInfo"`
+	MeasInfoID string     `xml:"measInfoId,attr"`
+	Job        job        `xml:"job"`
+	GranPeriod granPeriod `xml:"granPeriod"`
+	RepPeriod  repPeriod  `xml:"repPeriod"`
+	MeasType   []measType `xml:"measType"`
+	MeasValue  measValue  `xml:"measValue"`
+}
+type job struct {
+	XMLName xml.Name `xml:"job"`
+	//XMLAttr xml.Attr `xml:"jobId,attr"`
+	Key string `xml:"jobId,attr"`
+}
+type granPeriod struct {
+	XMLName xml.Name `xml:"granPeriod"`
+	Key     string   `xml:"duration,attr"`
+	Key2    string   `xml:"endTime,attr"`
+}
+type repPeriod struct {
+	XMLName xml.Name `xml:"repPeriod"`
+	Key     string   `xml:"duration,attr"`
+}
+type measType struct {
+	XMLName xml.Name `xml:"measType"`
+	Key     string   `xml:"p,attr"`
+	Value   string   `xml:",chardata"`
+}
+type measValue struct {
+	XMLName xml.Name `xml:"measValue"`
+	Key     string   `xml:"measObjLdn,attr"`
+	R       []r      `xml:"r"`
+}
+type r struct {
+	XMLName xml.Name `xml:"r"`
+	Key     string   `xml:"p,attr"`
+	Value   float64   `xml:",chardata"`
+}
+
+
+func init() {
+	registerCollector("pm", defaultEnabled, newPmCollector)
+}
+
+// NewUnameCollector returns new unameCollector.
+func newPmCollector(logger log.Logger) (Collector, error) {
+	return &pmCollector{logger}, nil
+}
+
+func (c *pmCollector) Update(ch chan<- prometheus.Metric) error {
+
+	// xml 파일 오픈
+	fp, err := os.Open("/home/thkim/myhfrlab/test-node-exporter/collector/data_parse.xml")
+	if err != nil {
+		panic(err)
+	}
+	defer fp.Close()
+
+	// xml 파일 읽기
+	data, err := ioutil.ReadAll(fp)
+
+	// xml 디코딩
+	var measDataFile measDataFile
+	err = xml.Unmarshal(data, &measDataFile)
+	if err != nil {
+		panic(err)
+	}
+
+	ch <- prometheus.MustNewConstMetric(pmDesc, prometheus.GaugeValue, measDataFile.MeasData.MeasInfo[0].MeasValue.R[0].Value,
+		measDataFile.FileFooter.XMLName.Local,
+	)
+
+	return nil
+}
